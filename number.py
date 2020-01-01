7d0c80c9724c028e7c744e9309551de6bdcb133c
Slater Rainney <44975996+srainney@users.noreply.github.com>
2020-01-01 16:46:54 -0500

    Completed the number guessing game

--- number.py
+++ number.py
@@ -1,4 +1,34 @@
+import random
+
 print('------------------------')
 print('    GUESS THE NUMBER')
 print('------------------------')
+print('')
 
+# Generates a random number between 0 and 100
+the_number = random.randint(1, 99)
+# Used to initiate the while statement
+guess = "text"
+the_number
+
+while guess != the_number:
+    # Checking if the supplied value is within the specified range
+    try:
+        guess = int(input('Guess a whole number between 0 and 100: '))
+    except ValueError:
+        print('That is not a whole number between 0 and 100. Please try again.')
+        continue
+    if 0 < guess < 100:
+        pass
+    else:
+        print('That is not a whole number between 0 and 100. Please try again.')
+        continue
+    # Prints the output for each guess
+    if guess < the_number:
+        print('Too low!')
+    elif guess > the_number:
+        print('Too high!')
+    else:
+        print('You win!')
+
+input('Press ENTER to exit')
